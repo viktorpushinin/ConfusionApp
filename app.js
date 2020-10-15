32302ca910193878f315cd355aeb47bfe2416b84
viktorpushinin <viktorpushinin@gmail.com>
2020-10-15 14:54:20 +0300

    concurrently run client and server

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
 var passport = require('passport');
-var authenticate = require('./authenticate');
 var config = require('./config');
 
 var indexRouter = require('./routes/index');
@@ -19,8 +15,6 @@ var favoriteRouter = require('./routes/favoriteRouter');
 var commentRouter = require('./routes/commentRouter');
 
 const mongoose = require('mongoose');
-
-const Dishes = require('./models/dishes');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
